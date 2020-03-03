a5db7cad131e1587d343717d5cd25e7c866a5ecf
alexawl <alexawl@bellsouth.net>
2020-03-02 19:15:51 -0500

    new file

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
-from application import app
+from application import app, db
 from flask import render_template, request, json, Response
+from application.models import User, Course, Enrollment
 
 courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
@@ -15,7 +16,7 @@ def login():
 
 @app.route("/courses/")
 @app.route("/courses/<term>")
-def courses(term="Spring 2020"):
+def courses(term="Spring 2019"):
     return render_template("courses.html", courseData=courseData, courses = True, term=term )
 
 @app.route("/register")
@@ -38,3 +39,17 @@ def api(idx=None):
         jdata = courseData[int(idx)]
     
     return Response(json.dumps(jdata), mimetype="application/json")
+
+class User(db.Document):
+    user_id     =   db.IntField( unique=True )
+    first_name  =   db.StringField( max_length=50 )
+    last_name   =   db.StringField( max_length=50 )
+    email       =   db.StringField( max_length=30 )
+    password    =   db.StringField( max_length=30 )
+
+@app.route("/user")
+def user():
+     User(user_id=1, first_name="Alejandro", last_name="Araya", email="alexawl@bellsouth.net", password="Maligator001").save()
+     User(user_id=2, first_name="Jennider", last_name="Gigler", email="h2opolo40@yahoo.com", password="Maligator001").save()
+     users = User.objects.all()
+     return render_template("user.html", users=users)
